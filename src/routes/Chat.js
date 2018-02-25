d3a6231611dc1332d1b6986b12d44a138206dc2b
Nick Filatov <keddofilatov@gmail.com>
2018-02-25 18:53:27 +0600

    [+] chat components folder

--- src/routes/Chat.js
+++ src/routes/Chat.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Header, AppLayout } from '../components/chat';
+import styled from 'styled-components';
 import SideBar from '../components/SideBar';
 import ChatContent from '../components/ChatContent';
 
@@ -10,5 +10,19 @@ const Chat = props => (
     <ChatContent {...props} />
   </AppLayout>
 );
+
+export const AppLayout = styled.div`
+  display: grid;
+  height: 100vh;
+  grid-template-columns: 250px 1fr;
+  grid-template-rows: auto 1fr auto;
+`;
+
+export const Header = styled.div`
+  grid-column: 2;
+  grid-row: 1;
+  background-color: #00999982;
+  height: 60px;
+`;
 
 export default Chat;
