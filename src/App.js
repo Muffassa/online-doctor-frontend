18a8b73167bec61ff79b723c07173b0af6d7e335
Nick Filatov <keddofilatov@gmail.com>
2018-03-03 12:13:27 +0600

    [+] websockets

--- src/App.js
+++ src/App.js
@@ -4,11 +4,32 @@ import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import 'semantic-ui-css/semantic.min.css';
+import { split } from 'apollo-link';
+import { WebSocketLink } from 'apollo-link-ws';
+import { getMainDefinition } from 'apollo-utilities';
 
 import Router from './routes';
 
+const wsLink = new WebSocketLink({
+  uri: 'ws://localhost:8081/subscriptions',
+  options: {
+    reconnect: true,
+  },
+});
+
+const httpLink = new HttpLink({ uri: 'http://localhost:8081/graphql', credentials: 'include' });
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return kind === 'OperationDefinition' && operation === 'subscription';
+  },
+  wsLink,
+  httpLink,
+);
+
 const client = new ApolloClient({
-  link: new HttpLink({ uri: 'http://localhost:8081/graphql', credentials: 'include' }),
+  link,
   cache: new InMemoryCache(),
 });
 
